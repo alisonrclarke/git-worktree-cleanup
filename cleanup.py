1f0163a1fb46fc238518e545185820cd749fe6f1
Alison Clarke <a.clarke@wellcome.org>
2026-03-26 16:27:44 +0000

    Updates to clean merged branches too and to check for squashed-and-rebased branches

--- cleanup.py
+++ cleanup.py
@@ -1,13 +1,12 @@
 import os
 import shutil
-import smtpd
 import subprocess
 import sys
 import re
 from enum import Enum
 from textwrap import dedent
 
-from git import Repo, InvalidGitRepositoryError
+from git import Repo, InvalidGitRepositoryError, NoSuchPathError
 
 from prompt_toolkit import prompt, choice
 from prompt_toolkit.completion import PathCompleter
@@ -24,8 +23,9 @@ class SubdirStatus(Enum):
 def _run_git_cmd(path, cmd, *args):
     result = subprocess.run(['git', cmd] + list(args),
                    check=True,
-                   cwd=path)
-    return result
+                   cwd=path,
+                            capture_output=True)
+    return result.stdout.decode('utf-8')
 
 
 def _open_shell(working_dir):
@@ -49,7 +49,7 @@ def _open_shell(working_dir):
     process.wait()
 
 
-def _iterate_options(worktree_dir: str, subdir: str, subdir_path: str, subdir_status: SubdirStatus):
+def _iterate_options(worktree_dir: str, subdir: str, subdir_path: str, subdir_status: SubdirStatus, subdir_branch: str=None):
     next_step = None
 
     match subdir_status:
@@ -63,8 +63,8 @@ def _iterate_options(worktree_dir: str, subdir: str, subdir_path: str, subdir_st
     while next_step != 'ignore':
         next_step = choice(
             message='What would you like to do?',
-            options=[('explore', 'Open shell to explore changes'),
-                     ('ignore', 'Ignore this worktree and continue cleanup'),
+            options=[('ignore', 'Ignore this worktree and continue cleanup'),
+                     ('explore', 'Open shell to explore changes'),
                      ('delete', delete_option)],
         )
 
@@ -85,14 +85,43 @@ def _iterate_options(worktree_dir: str, subdir: str, subdir_path: str, subdir_st
                 git_cmd_args.append(subdir)
 
                 if confirm(delete_msg):
-                    _run_git_cmd(worktree_dir, 'worktree', *git_cmd_args)
+                    print(_run_git_cmd(worktree_dir, 'worktree', *git_cmd_args))
+                    print(_run_git_cmd(worktree_dir, 'branch', "-D", subdir_branch))
                     next_step = 'ignore'
 
         elif next_step == 'explore':
             _open_shell(subdir_path)
 
 
+def _is_branch_merged(repo: Repo, default_branch: str, worktree_dir: str, branch: str, branch_commit: str) -> bool:
+    contains_output = repo.git.branch('--contains', branch_commit)
+    containing_branches = re.findall(r'([\w\-]+)', contains_output)
+
+    # TODO: use remote not local branch
+    if default_branch in containing_branches:
+        print(f"{branch} at commit {branch_commit} has been merged to {default_branch}")
+        merged = True
+    else:
+        # Commands here taken from answer to:
+        # https://stackoverflow.com/questions/43489303/how-can-i-delete-all-git-branches-which-have-been-squash-and-merge-via-github
+        merge_base = _run_git_cmd(worktree_dir, "merge-base", default_branch, branch).rstrip()
+        rev_parse_hash = _run_git_cmd(worktree_dir, "rev-parse", f"{branch}^{{tree}}").rstrip()
+        commit_tree_hash = _run_git_cmd(worktree_dir, "commit-tree", rev_parse_hash, "-p", merge_base, "-m",
+                                        "_").rstrip()
+        cherry_output = _run_git_cmd(worktree_dir, "cherry", default_branch, commit_tree_hash)
+
+        if cherry_output.startswith('-'):
+            print(f"{branch} at commit {branch_commit} has been squashed-and-rebased to {default_branch}")
+            merged = True
+        else:
+            merged = False
+    return merged
+
+
 def main():
+
+    subdirs = None
+
     if len(sys.argv) == 1:
         path_completer = PathCompleter(
             only_directories=True,
@@ -101,6 +130,8 @@ def main():
         worktree_dir = prompt("Select a directory that uses git-worktree: ", default='~/', completer=path_completer)
     else:
         worktree_dir = sys.argv[1]
+        if len(sys.argv) > 2:
+            subdirs = [sys.argv[2]]
 
     print(f"Checking worktree {worktree_dir}")
 
@@ -112,8 +143,6 @@ def main():
         print(f"Error accessing repository: {e}")
         sys.exit(1)
 
-    # TODO check if this is a worktree?
-
     repo.remote().fetch(prune=True)
 
     default_branch_pattern = re.compile(r'^\*|master|main|dev')
@@ -123,24 +152,15 @@ def main():
 
     default_branch = choice(message="Select default remote branch: ", default='develop', options=[(b, b) for b in default_branches])
 
-    local_branches = {h.name: h for h in repo.heads}
-    branch_names = sorted(local_branches.keys())
+    if subdirs is None:
+        subdirs = sorted([
+            d for d in os.listdir(worktree_dir)
+            if os.path.isdir(os.path.join(worktree_dir, d)) and not d.startswith('.')
+               and not re.match(r'^\*|master|main|dev', d)
+        ])
+        subdirs.sort()
 
-    subdirs = sorted([
-        d for d in os.listdir(worktree_dir)
-        if os.path.isdir(os.path.join(worktree_dir, d)) and not d.startswith('.')
-           and not re.match(r'^\*|master|main|dev', d)
-    ])
-    subdirs.sort()
-
-    print(f"Found {len(subdirs)} subdirectories")
-
-    # merged_branch_text = repo.git.branch('--merged', f'origin/{default_branch}')
-    # merged_branches = re.findall(r'([\w\-]+)', merged_branch_text)
-    #
-    # print(f"Found {len(branch_names)} local branches and {len(merged_branches)} merged branches")
-
-    # default_branch_ref = repo.remote().refs[default_branch]
+        print(f"Found {len(subdirs)} subdirectories")
 
     for subdir in subdirs:
         print(f"\nChecking subdir {subdir}")
@@ -149,26 +169,24 @@ def main():
 
         try:
             worktree_repo = Repo(subdir_path)
+        except NoSuchPathError:  # Could happen if path passed on command line
+            print(f"{subdir_path} is not a valid path")
+            continue
         except InvalidGitRepositoryError:
             print(f"{subdir} is not a valid git worktree")
             _iterate_options(worktree_dir, subdir, subdir_path, SubdirStatus.NOT_A_WORKTREE)
-
             continue
 
-        subdir_branch = worktree_repo.active_branch.name
+        try:
+            subdir_branch = worktree_repo.active_branch.name
+        except ValueError:
+            print(f"Unable to get current branch for {subdir}")
+            _iterate_options(worktree_dir, subdir, subdir_path, SubdirStatus.NOT_A_WORKTREE)
+            continue
 
         print(f"Subdir {subdir} has active branch {subdir_branch}")
 
-        contains_output = repo.git.branch('--contains', worktree_repo.commit().hexsha)
-        containing_branches = re.findall(r'([\w\-]+)', contains_output)
-
-        # TODO: use remote not local branch
-        if default_branch in containing_branches:
-            print(f"{subdir_branch} at commit {worktree_repo.commit()} has been merged to {default_branch}")
-            merged = True
-        else:
-            # TODO Check if branch has been squashed-and-rebased
-            merged = False
+        merged = _is_branch_merged(repo, default_branch, worktree_dir, subdir_branch, worktree_repo.commit().hexsha)
 
         if merged:
             modified_files = worktree_repo.index.diff(None)
@@ -177,7 +195,7 @@ def main():
             if modified_files or untracked_files:
                 subdir_status = SubdirStatus.MERGED_DIRTY
                 print(f"There are {len(modified_files)} changed files and  {len(untracked_files)} changes in {subdir}")
-                _run_git_cmd(subdir_path, 'status')
+                print(_run_git_cmd(subdir_path, 'status'))
             else:
                 subdir_status = SubdirStatus.MERGED_CLEAN
 
@@ -185,7 +203,18 @@ def main():
             print(f"{subdir_branch} at commit {worktree_repo.commit()} has not been merged to {default_branch}")
             subdir_status = SubdirStatus.UNMERGED
 
-        _iterate_options(worktree_dir, subdir, subdir_path, subdir_status)
+        _iterate_options(worktree_dir, subdir, subdir_path, subdir_status, subdir_branch)
+
+    # Now we've looped over subdirs, check for remaining unmerged local branches
+    local_branches = {h.name: h for h in repo.heads if not default_branch_pattern.match(h.name) }
+
+    if len(sys.argv) > 2:
+        local_branches = {k: v for k, v in local_branches.items() if k == sys.argv[2]}
+
+    for branch_name, head in local_branches.items():
+        merged = _is_branch_merged(repo, default_branch, worktree_dir, branch_name, head.commit.hexsha)
+        if merged and confirm(f"Delete merged local branch {branch_name}?"):
+            print(_run_git_cmd(worktree_dir, 'branch', "-D", branch_name))
 
 
 if __name__ == "__main__":
